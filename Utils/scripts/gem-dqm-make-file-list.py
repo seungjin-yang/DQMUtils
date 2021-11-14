566bb9f19599b6c57918f4c1f3c4eacb8d54eaba
Seungjin Yang <seungjin.yang@cern.ch>
2021-11-14 15:42:05 +0900

    fix a bug

--- Utils/scripts/gem-dqm-make-file-list.py
+++ Utils/scripts/gem-dqm-make-file-list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 r"""
+TODO
+- [ ] how about making a dict like storage.xml in the SITECONF
 """
 from pathlib import Path
 import socket
@@ -10,11 +12,16 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_dir', type=Path)
     parser.add_argument('-p', '--pathname', type=str, default='*.root')
-    parser.add_argument('-o', '--output-file', type=str)
+    parser.add_argument('-o', '--output-path', type=str)
     args = parser.parse_args()
 
     if not args.input_dir.is_dir():
         raise IsADirectoryError(args.input_dir)
+
+    if args.output_path is None:
+        args.output_path = Path.cwd().joinpath(f'filelist-{args.input_dir.name}').with_suffix('.txt')
+    if args.output_path.exists():
+        raise FileExistsError(args.output_path)
 
     file_list = args.input_dir.glob(args.pathname)
     file_list = [str(each) for each in file_list]
@@ -29,12 +36,7 @@ def main():
         raise NotImplementedError(hostname)
 
     text = '\n'.join(file_list)
-
-    output_path = Path.cwd().joinpath(f'filelist-{args.input_dir.name}').with_suffix('.txt')
-    if output_path.exists():
-        raise FileExistsError(output_path)
-
-    with open(output_path, 'w') as txt_file:
+    with open(args.output_path, 'w') as txt_file:
         txt_file.write(text)
 
 
